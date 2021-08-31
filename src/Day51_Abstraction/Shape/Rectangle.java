79938b3b0956e1b4123b43dc5dec1b201365a5db
karina quezada <quezadacore32@gmail.com>
2021-08-31 12:45:26 -0400

    rectangle

--- src/Day51_Abstraction/Shape/Rectangle.java
+++ src/Day51_Abstraction/Shape/Rectangle.java
@@ -1,4 +1,48 @@
 package Day51_Abstraction.Shape;
 
-public class Rectangle {
+public class Rectangle extends Shape{
+
+
+    private double l, w;
+
+    public double getL() {
+        return l;
+    }
+
+    public void setL(double l) {
+        if(l <= 0){
+            throw new RuntimeException("Invalid Length: "+l);
+        }
+        this.l = l;
+    }
+
+    public double getW() {
+        return w;
+    }
+
+    public void setW(double w) {
+        if(w <= 0){
+            throw new RuntimeException("Invalid Width: "+w);
+        }
+        this.w = w;
+    }
+
+    public Rectangle(double l, double w) {
+        super("Rectangle");
+        setL(l);
+        setW(w);
+    }
+
+
+    @Override
+    public double area() {
+        return l * w;
+    }
+
+    @Override
+    public double perimeter() {
+        return (l+w)*2;
+    }
+
+
 }
