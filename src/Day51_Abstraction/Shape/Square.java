bc8d83cb0d3e6365bc54b1b680dc1b979a1dcdc3
karina quezada <quezadacore32@gmail.com>
2021-08-31 12:55:56 -0400

    first commit

---
+++ src/Day51_Abstraction/Shape/Square.java
@@ -0,0 +1,33 @@
+package Day51_Abstraction.Shape;
+
+
+public final class Square extends Shape{
+
+    private double s;
+
+    public double getS() {
+        return s;
+    }
+
+    public void setS(double s) {
+        if (s <= 0) {
+            throw new RuntimeException("Invalid Side: " + s);
+        }
+        this.s = s;
+    }
+
+    public Square(double s) {
+        super("Square");
+        setS(s);
+    }
+
+    @Override
+    public double area() {
+        return s * s;
+    }
+
+    @Override
+    public double perimeter() {
+        return s * 4;
+    }
+}
