3581c747a62e39520eb18a57116e926255c54b2a
karina quezada <quezadacore32@gmail.com>
2021-09-09 10:58:04 -0400

    day 53 WebDriver Task

---
+++ src/Day53_Abstraction/WebDriverTask/Automation.java
@@ -0,0 +1,61 @@
+package Day53_Abstraction.WebDriverTask;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Automation {
+
+    public static void main(String[] args) {
+
+        // ***************Chrome Driver********************
+
+        ChromeDriver driver =  new ChromeDriver();
+        driver.get("www.google.com");
+        driver.findElement("xpath");
+        driver.getTitle();
+        driver.close();
+        driver.quit();
+
+
+        System.out.println("----------------------------------------");
+
+        // ***************Firefox Driver********************
+
+        FirefoxDriver driver2 = new FirefoxDriver();
+        driver.get("www.google.com");
+        driver.findElement("xpath");
+        driver.getTitle();
+        driver.close();
+        driver.quit();
+
+
+        System.out.println("-----------------------------------------");
+
+        // ***************Opera Driver********************
+
+        OperaDriver driver3 = new OperaDriver();
+        driver3.get("www.google.com");
+        driver3.findElement("xpath");
+        driver3.getTitle();
+        driver3.close();
+        driver3.quit();
+
+        System.out.println("-----------------------------------------");
+
+        // *************** Edge Driver********************
+
+        EdgeDriver driver4 = new EdgeDriver();
+        driver4.get("www.google.com");
+        driver4.findElement("xpath");
+        driver4.getTitle();
+        driver4.close();
+        driver4.quit();
+
+        System.out.println("-----------------------------------------");
+
+
+
+    }
+
+
+
+}
---
+++ src/Day53_Abstraction/WebDriverTask/EdgeDriver.java
@@ -0,0 +1,32 @@
+package Day53_Abstraction.WebDriverTask;
+
+
+    public class EdgeDriver implements WebDriver {
+        @Override
+        public void get(String url) {
+            System.out.println("EdgeDriver - navigating to: "+url);
+        }
+
+        @Override
+        public void findElement(String locator) {
+            System.out.println("ChromeDriver - locating the element by "+locator);
+        }
+
+        @Override
+        public void getTitle() {
+            System.out.println("EdgeDriver - getting the title");
+        }
+
+        @Override
+        public void quit() {
+            System.out.println("EdgeDriver - quit");
+
+        }
+
+        @Override
+        public void close() {
+            System.out.println("EdgeDriver - closing the tab");
+        }
+
+    }
+
