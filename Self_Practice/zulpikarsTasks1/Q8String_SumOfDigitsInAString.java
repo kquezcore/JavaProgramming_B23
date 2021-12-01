8ac406020e63209360d0807d60dd1a7e332221a4
karina quezada <quezadacore32@gmail.com>
2021-12-01 13:45:11 -0500

    sum of digits finished

--- Self_Practice/zulpikarsTasks1/Q8String_SumOfDigitsInAString.java
+++ Self_Practice/zulpikarsTasks1/Q8String_SumOfDigitsInAString.java
@@ -1,5 +1,24 @@
 package zulpikarsTasks1;
 
 public class Q8String_SumOfDigitsInAString {
+
+    public static void main(String[] args) {
+     String s="12 some text 3 7";
+
+        System.out.println(getSumOfNumbers(s));
+    }
+
+    public static int getSumOfNumbers(String s) {
+
+    int sum = 0;
+
+    String[] numbers = s.replaceAll("[\\D]+"," ").split(" ");
+
+        for(String each : numbers){
+        sum+=Integer.parseInt(each);
+    }
+
+        return sum;
+}
 }
 // write a method that can return the sum of the digits in a string
