c08eb5a1b2648a2ad369b1eb24a250820433cdc0
karina quezada <quezadacore32@gmail.com>
2021-12-01 12:45:23 -0500

    remove duplicates finished

--- Self_Practice/zulpikarsTasks1/Q3String_RemoveDuplicates.java
+++ Self_Practice/zulpikarsTasks1/Q3String_RemoveDuplicates.java
@@ -1,6 +1,25 @@
 package zulpikarsTasks1;
 
 public class Q3String_RemoveDuplicates {
+    public static void main(String[] args) {
+        String word= "aaabbbccc";
+
+        Q3String_RemoveDuplicates removeDuplicates= new Q3String_RemoveDuplicates();
+        System.out.println(removeDuplicates.removeDup(word));
+
+    }
+
+    public String removeDup(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains("" + str.charAt(i))) {
+                result += "" + str.charAt(i);
+            }
+        }
+
+        return result;
+    }
 
 }
 // write a return method that can remove the duplicated values from string
