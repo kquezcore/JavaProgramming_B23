46f39a4157433337bb8a08471bd0f24331a7b474
karina quezada <quezadacore32@gmail.com>
2021-12-01 12:45:37 -0500

    move all zeros to the end finished

--- Self_Practice/zulpikarsTasks1/Q15Array_MoveZerosToTheEnd.java
+++ Self_Practice/zulpikarsTasks1/Q15Array_MoveZerosToTheEnd.java
@@ -1,5 +1,30 @@
 package zulpikarsTasks1;
 
+import java.util.Arrays;
+
 public class Q15Array_MoveZerosToTheEnd {
+    public static void main(String[] args) {
+
+        int [] array= {2,0,0,4,0,9};
+
+    Q15Array_MoveZerosToTheEnd moveZerosToTheEnd= new Q15Array_MoveZerosToTheEnd();
+
+        System.out.println(Arrays.toString(moveZerosToTheEnd.moveZs(array)));
+        }
+    public int[] moveZs(int[] array) {
+
+        int[] result = new int[array.length];
+        int count = 0;
+
+        for (int each : array) {
+            if (each != 0)
+                result[count++] = each;
+        }
+
+        return result;
+
+    }
+
+
 }
 // Write a method that can move all the zeros to the last indexes of the array(do not use sort method)
