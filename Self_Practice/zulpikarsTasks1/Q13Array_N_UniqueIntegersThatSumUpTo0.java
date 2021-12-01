9fc9069c8b56006361a9290a3e9f124aaeb31b82
karina quezada <quezadacore32@gmail.com>
2021-12-01 13:17:43 -0500

    sum up to zero

--- Self_Practice/zulpikarsTasks1/Q13Array_N_UniqueIntegersThatSumUpTo0.java
+++ Self_Practice/zulpikarsTasks1/Q13Array_N_UniqueIntegersThatSumUpTo0.java
@@ -1,6 +1,43 @@
 package zulpikarsTasks1;
 
+import java.util.Arrays;
+
 public class Q13Array_N_UniqueIntegersThatSumUpTo0 {
+
+    public static void main(String[] args) {
+
+        int [] nums= {20,30,1,4};
+        System.out.println(Arrays.toString(sumZero(nums.length)));
+
+
+    }
+    public static int[] sumZero(int n) {
+        int[] result = new int[n];
+        if (n == 1) {
+            result[0] = 0;
+            return result;
+        }
+        int x = n / 2;
+        if (n % 2 == 0) {
+            int i = 0;
+            while (i < n) {
+                result[i] = x;
+                result[i + 1] = -x;
+                i += 2;
+                x--;
+            }
+        } else {
+            result[0] = 0;
+            int i = 1;
+            while (i < n) {
+                result[i] = x;
+                result[i + 1] = -x;
+                i += 2;
+                x--;
+            }
+        }
+        return result;
+    }
 }
 /*
 WRITE A FUNCTION:
