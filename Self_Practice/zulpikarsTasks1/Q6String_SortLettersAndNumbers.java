13db92814ff485cd3e90c853f92850fc077de624
karina quezada <quezadacore32@gmail.com>
2021-12-01 13:40:06 -0500

    sort letters finished

--- Self_Practice/zulpikarsTasks1/Q6String_SortLettersAndNumbers.java
+++ Self_Practice/zulpikarsTasks1/Q6String_SortLettersAndNumbers.java
@@ -1,9 +1,49 @@
 package zulpikarsTasks1;
 
+import java.util.Arrays;
+
 public class Q6String_SortLettersAndNumbers {
-}
+        static String actual = "DC501GCCA098911";
+        static String expected = "CD015ACCG011899";
+
+        public static void main(String[] args){
+            String firstPart = actual.substring(0,2);
+            String secondPart = actual.substring(2,5);
+            String thirdPart = actual.substring(5,9);
+            String fourthPart = actual.substring(9);
+
+            char[] firstPartArr = firstPart.toCharArray();
+            Arrays.sort(firstPartArr);
+            char[] secondPartArr = secondPart.toCharArray();
+            Arrays.sort(secondPartArr);
+            char[] thirdPartArr = thirdPart.toCharArray();
+            Arrays.sort(thirdPartArr);
+            char[] fourthPartArr = fourthPart.toCharArray();
+            Arrays.sort(fourthPartArr);
+
+            actual = "";
+
+            for(char c:firstPartArr){
+                actual+=c;
+            }
+            for(char c:secondPartArr){
+                actual+=c;
+            }
+            for(char c:thirdPartArr){
+                actual+=c;
+            }
+            for(char c:fourthPartArr){
+                actual+=c;
+            }
+
+            System.out.println(actual);
+        }
+
+    }
+
 //sort letters and numbers form alphanumeric string
-// given alphanumeric string, we need to split the stinf itno substrings of consecutive letters or numbers, sort the individual string
+// given alphanumeric string, we need to split the stinf itno substrings of consecutive letters or numbers,
+// sort the individual string
 // and append them back together
 // ex: input: :"DC501GCCCA098911"
 //     OUTPUT:  "CD015ACCCG11899
