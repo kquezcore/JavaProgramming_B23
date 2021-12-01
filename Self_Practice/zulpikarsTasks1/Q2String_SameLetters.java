ab626aa2c68501866e3adadf05e02a1b084f2516
karina quezada <quezadacore32@gmail.com>
2021-12-01 12:45:06 -0500

    same letters finished

--- Self_Practice/zulpikarsTasks1/Q2String_SameLetters.java
+++ Self_Practice/zulpikarsTasks1/Q2String_SameLetters.java
@@ -1,6 +1,24 @@
 package zulpikarsTasks1;
 
 public class Q2String_SameLetters {
+    public static void main(String[] args) {
+       String name= "abc";
+       String name2= "cab";
+
+
+        System.out.println(sameLetters(name,name2));
+    }
+
+    public static boolean sameLetters(String word1, String word2){
+        boolean result= true;
+
+        if (word1.contains(word2)){
+            result= true;
+        }else{
+            result = false;
+        }
+        return result;
+            }
 }
 // Write Return method that check if a String us build out of the same letters as another string.
 //ex: same("abc","cab"): == true
